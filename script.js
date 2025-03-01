10a8c7339c6dbcd6bcfb270c8ae224788f398d03
nicomat0 <122412614+nicomat0@users.noreply.github.com>
2025-02-28 21:48:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,14 +32,38 @@ document.addEventListener("DOMContentLoaded", function () {
   window.onload = typeWriter;
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('.menu');
-  
-  // Toggle menu visibility
-  menuToggle.addEventListener('click', function () {
-    menu.classList.toggle('active');
-    menuToggle.classList.toggle('menu-open');
+$(document).ready(function () {
+  $(".menu-toggle").click(function () {
+    $(".menu-toggle").toggleClass("menu-open");
+    $(".menu").toggleClass("active");
+  });
+  $(".link").click(function () {
+    $(".menu-toggle").toggleClass("menu-open");
+    $(".menu").toggleClass("active");
+  });
+  $(".videoOne").click(function () {
+    $(".overlayOne").toggleClass("active");
+  });
+  $(".videoTwo").click(function () {
+    $(".overlayTwo").toggleClass("active");
+  });
+  $(".videoThree").click(function () {
+    $(".overlayThree").toggleClass("active");
+  });
+  $(".videoFour").click(function () {
+    $(".overlayFour").toggleClass("active");
+  });
+  $(".closeOverlayOne").click(function () {
+    $(".overlayOne").toggleClass("active");
+  });
+  $(".closeOverlayTwo").click(function () {
+    $(".overlayTwo").toggleClass("active");
+  });
+  $(".closeOverlayThree").click(function () {
+    $(".overlayThree").toggleClass("active");
+  });
+  $(".closeOverlayFour").click(function () {
+    $(".overlayFour").toggleClass("active");
   });
 });
 
